e8a0f81cb1601314af3764f81556d4c2382c41da
Zac Sanders <zacharyjsanders@gmail.com>
2020-12-23 12:13:50 -0500

    Adds search styling and fixes

--- wp-content/themes/crimson/search.php
+++ wp-content/themes/crimson/search.php
@@ -1,38 +1,50 @@
 <?php
+
 get_header();
-if( have_posts() ):
-  ?>
+
+if( have_posts() ): ?>
+
   <header class="page-header">
-    <h1 class="page-title">
+    <h1 class="page-title text-center mb-5">
       <?php printf( esc_html__( 'Search Results for: %s', 'crimson' ), '<span>' . get_search_query() . '</span>' ); ?>
     </h1>
   </header>
-  <?php
-  while(have_posts()):
-    the_post();
-    ?>
-    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-      <header class="entry-header">
-        <?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
-        <?php if ( 'post' === get_post_type() ) : ?>
-          <div class="entry-meta">
-              <?php crimson_posted_on(); ?>
-              <?php crimson_posted_by(); ?>
-          </div>
-        <?php endif; ?>
-      </header>
-      <?php crimson_post_thumbnail(); ?>
-      <div class="entry-summary">
-        <?php the_excerpt(); ?>
+    <div class="container">
+      <div class="row">
+  
+      <?php while(have_posts()): the_post(); ?>
+
+        <div class="col-sm-6 col-md-4">
+          <article class="search-tile" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+            <header class="entry-header">
+              <?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+              <?php if ( 'post' === get_post_type() ) : ?>
+                <div class="entry-meta">
+                    <?php crimson_posted_on(); ?>
+                    <?php crimson_posted_by(); ?>
+                </div>
+              <?php endif; ?>
+            </header>
+            <?php crimson_post_thumbnail(); ?>
+            <div class="entry-summary">
+              <?php the_excerpt(); ?>
+            </div>
+            <footer class="entry-footer">
+              <?php crimson_entry_footer(); ?>
+            </footer>
+          </article>
+        </div> 
+      
+      <?php endwhile; ?>
+
       </div>
-      <footer class="entry-footer">
-        <?php crimson_entry_footer(); ?>
-      </footer>
-    </article>
+    </div>
+
     <?php
-  endwhile;
-  the_posts_navigation();
-else:
-  get_template_part( 'template-parts/content', 'none' );
-endif;
+  
+    the_posts_navigation();
+    else:
+      get_template_part( 'template-parts/content', 'none' );
+    endif;
+
 get_footer();
