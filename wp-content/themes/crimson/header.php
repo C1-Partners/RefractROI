5b8d346540776827503774656cb5d7e39ee0acbd
Zac Sanders <zacharyjsanders@gmail.com>
2020-12-23 11:36:25 -0500

    Adds fix for nav and button styling

--- wp-content/themes/crimson/header.php
+++ wp-content/themes/crimson/header.php
@@ -55,7 +55,7 @@ $ctaLink = get_field('cta_link', 'options');
 					<form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ) ?>">
 						<label class="sr-only" for="site-search-field">Search for</label>
 						<input type="search" id="site-search-field" class="search-field" placeholder="Search <?php bloginfo( 'name' ) ?>&hellip;" value="<?php echo get_search_query() ?>" name="s" />
-						<input type="submit" class="search-submit btn-primary btn-red" value="Go" />
+						<input type="submit" class="search-submit search-btn" value="Go" />
 					</form>
 				</div>
 				<div class="site-actions">
