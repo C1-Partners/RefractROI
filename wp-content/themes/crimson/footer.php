7386c5e9d373ef727b101cf4cc88110274be618f
Zac Sanders <zacharyjsanders@gmail.com>
2021-01-22 09:27:47 -0700

    Adds hire and expert button to footer

--- wp-content/themes/crimson/footer.php
+++ wp-content/themes/crimson/footer.php
@@ -1,10 +1,28 @@
-	
+<?php
+
+$link = get_field('expert_btn', 'options');
+
+if( $link ): 
+    $link_url = $link['url'];
+    $link_title = $link['title'];
+    $link_target = $link['target'] ? $link['target'] : '_self';
+endif; 
+
+?>
 		
 		<?php get_template_part( 'template-parts/page/content', 'footer' ); ?>
 
 		</main>
 		
 		<?php wp_footer(); ?>
+
+		<?php if( $link ): ?>
+			<a class="btn-primary btn-float" role="link" 
+				href="<?php echo esc_url( $link_url ); ?>" 
+				target="<?php echo esc_attr( $link_target ); ?>">
+				<span><?php echo esc_html( $link_title ); ?></span>
+			</a>
+		<?php endif; ?>
 		
 	</body>
 </html>
