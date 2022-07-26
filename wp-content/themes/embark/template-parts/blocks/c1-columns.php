7ae727f3cdd1467533b8efa55a371561b8c429d0
Zac Sanders <zacharyjsanders@gmail.com>
2022-07-26 11:29:37 -0400

    upgrades columns block

--- wp-content/themes/embark/template-parts/blocks/c1-columns.php
+++ wp-content/themes/embark/template-parts/blocks/c1-columns.php
@@ -8,9 +8,12 @@ $block_prefix = 'cb';
 $block_style = get_field('cb_stl');
 $col_style = get_field('col_stl');
 $heading = get_field('cb_hd');
+$text_top = get_field('cb_txt1');
+$text_bot = get_field('cb_txt2');
 $items = get_field('cb_cols');
 $link = get_field('cb_lnk');
 $link_style = get_field('cb_lnkstl');
+$emp_hd = get_field('cb_emp');
 
 ?>
 
@@ -18,7 +21,10 @@ $link_style = get_field('cb_lnkstl');
     <?php if ($heading): ?>
     <h2 class="h1"><?php echo $heading; ?></h2>
     <?php endif; ?>
-    <div class="<?php echo $block_prefix; ?>__cols">
+    <?php if ($text_top): ?>
+    <p class="<?php echo $block_prefix; ?>__text"><?php echo $text_top; ?></p>
+    <?php endif; ?>
+    <div class="<?php echo $block_prefix; ?>__cols <?php echo ($emp_hd) ? 'emp' : '';?>">
     <?php 
         if (is_array($items)) {
             foreach ($items as $item) {
@@ -65,7 +71,11 @@ $link_style = get_field('cb_lnkstl');
                 }
             } 
         } ?>
+        
     </div>
+    <?php if ($text_bot): ?>
+    <p class="<?php echo $block_prefix; ?>__text"><?php echo $text_bot; ?></p>
+    <?php endif; ?>
     <?php if ($link) : ?>
     <div class="<?php echo $block_prefix; ?>__cta btn-row">
         <a class="<?php echo ($link_style); ?>" 
