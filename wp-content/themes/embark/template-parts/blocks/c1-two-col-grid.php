2d7d1d171b3daab7361ef70dc8b2d497d756d1c5
Zac Sanders <zacharyjsanders@gmail.com>
2022-08-09 10:22:03 -0400

    updates grid block

--- wp-content/themes/embark/template-parts/blocks/c1-two-col-grid.php
+++ wp-content/themes/embark/template-parts/blocks/c1-two-col-grid.php
@@ -5,14 +5,15 @@ $cont_left = get_field('cont_l');
 $cont_rght = get_field('cont_r');
 $icon = get_field('tcg_icn');
 $txt_clr = get_field('tcg_txclr');
-// $cta_text = get_field('pgt_txt');
-// $link = get_field('pgt_lnk');
-// $link_style = get_field('pgt_lnkstl');
+$bg_img = get_field('tcg_img');
+$img_ord = (empty(get_field('tcg_ord')) || get_field('tcg_ord') == 1) ? 'order-1' : 'order-2';
+$cont_ord = (get_field('tcg_ord') == 1) ? 'order-2' : 'order-1';
+$link = get_field('tcg_lnk');
 
 ?>
 
 <section class="<?php echo $block_prefix;?>">
-    <div class="<?php echo $block_prefix; ?>__left <?php echo $txt_clr; ?>">
+    <div class="<?php echo $block_prefix; ?>__col-1 <?php echo $txt_clr; ?> <?php echo $cont_ord; ?>" <?php echo ($bg_img) ? 'style="background-image:url('.$bg_img['url'].')"' : '';?>>
         <?php if ($cont_left) {
             echo $cont_left;
         } ?>
@@ -25,13 +26,28 @@ $txt_clr = get_field('tcg_txclr');
                         "alt" => $icon['title']
                     ],
                     "style" => ["type" => "standard"]
-                ]); ?>
+                ]); 
+            ?>
             </div>
        <?php } ?>
     </div>
-    <div class="<?php echo $block_prefix; ?>__right">
-    <?php if ($cont_rght) {
+    <div class="<?php echo $block_prefix; ?>__col-2 <?php echo $img_ord; ?>"> 
+    <div class="<?php echo $block_prefix; ?>__inner">
+        <?php if ($cont_rght) {
             echo $cont_rght;
         } ?>
+    </div>
+    <?php if ($link): ?>
+        <?php 
+            echo gsc("link", [
+                "content" => [
+                    "acf_link" => $link,
+                ],
+                "style" => [
+                    "class" => 'arrow',
+                    ]
+            ]);
+        ?>
+    <?php endif; ?>  
     </div>
 </section>
